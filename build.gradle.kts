e905c4179d585dabebab76608033404264c2b0ff
pichs <sywubo789@163.com>
2026-01-22 14:34:00 +0800

    demo

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,6 @@ ext {
     set("minSdk", 26)
     set("targetSdk", 35)
     set("javaVersion", "17")
-    set("versionName", "2.1.4")
+    set("versionName", "2.1.5")
     set("versionCode", calculateVersionCode((get("versionName") ?: "1.0.0").toString()))
 }
